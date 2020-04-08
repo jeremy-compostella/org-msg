122ffba8298734f242c862cf052efc4ad9f49833
Jeremy Compostella <jeremy.compostella@gmail.com>
2020-04-08 16:35:02 -0700

    2.6
    Signed-off-by: Jeremy Compostella <jeremy.compostella@gmail.com>

--- org-msg.el
+++ org-msg.el
@@ -6,7 +6,7 @@
 ;; Created: January 2018
 ;; Keywords: extensions mail
 ;; Homepage: https://github.com/jeremy-compostella/org-msg
-;; Package-Version: 2.5
+;; Package-Version: 2.6
 ;; Package-Requires: ((emacs "24.4") (htmlize "1.54"))
 
 ;; This program is free software; you can redistribute it and/or modify
